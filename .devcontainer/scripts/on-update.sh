111ca89ef36454f71d01ab28acfbdc7d7301e274
Sasha Koss <koss@nocorp.me>
2025-06-23 01:25:16 +0000

    Rework the dev container setup

--- .devcontainer/scripts/on-update.sh
+++ .devcontainer/scripts/on-update.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# This script is run after the container is updated.
+
 set -e
 
-# Nothing here yet, but that's the place for running stuff like `mise install`
+eval "$(mise activate bash --shims)"
+
+# Update mise
+mise self-update -y
